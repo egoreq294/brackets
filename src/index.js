500c4bcbda73b5c640ca230ee265a84c8677ec72
egoreq <egoreq@bk.ru>
2020-03-09 14:41:27 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrstr = str.split( '' );
+
+    for (let i = 0; i < arrstr.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if (arrstr[i] === bracketsConfig[j][0] && arrstr[i + 1] === bracketsConfig[j][1]) {
+                arrstr.splice(i, 2);
+                i = -1;
+            }
+		}
+		if (arrstr.length === 0){
+            return true;
+        }
+	}
+
+	return false;
 }
